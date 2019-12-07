9bca71968bdcf2aa91f2ce9bd46cec9b87591d47
S-toe <58405725+S-toe@users.noreply.github.com>
2019-12-07 14:21:16 +0700

    Update LocalPlayer.java

--- src/com/BlockSnake/game/LocalPlayer.java
+++ src/com/BlockSnake/game/LocalPlayer.java
@@ -9,30 +9,39 @@ import com.BlockSnake.entities.Snake;
 public class LocalPlayer extends GameMode implements Runnable {
 
 	//entities
-	ArrayList<Snake> snakes;
+//	public static ArrayList<Snake> snakes;
+	public static Snake P1;
+	public static Snake P2;
 	Food food;
 	
 	//game properties
 	public static boolean running = false;
 	public static boolean paused = false;
-//	public static int dead = 0;
+	public static boolean gameover = false;
+	public static boolean dead1 = false;
+	public static boolean dead2 = false;
 	static int gameSpeed = 100000000;
 	
 	public LocalPlayer() {
 		
-		snakes = new ArrayList<Snake>();
-		snakes.add(new Snake());
-		snakes.add(new Snake(480, 480, Direction.left));
+//		snakes = new ArrayList<Snake>();
+//		snakes.add(new Snake());
+//		snakes.add(new Snake(480, 480, Direction.left));
+		P1 = new Snake();
+		P2 = new Snake(480, 480, Direction.left);
 		food = new Food();
 	}
 	
 	public void reset() {
 		
-		snakes.get(0).reset();
-		snakes.get(1).reset();
+//		snakes.get(0).reset();
+//		snakes.get(1).reset();
+		P1.reset();
+		P2.reset();
 		food.reset();
 		running = false;
 		paused = false;
+		gameover = false;
 	}
 	
 	@Override
@@ -42,45 +51,62 @@ public class LocalPlayer extends GameMode implements Runnable {
 		running = true;
 		
 		while(running) {
-			if(!paused) {
-				if(System.nanoTime() - last > gameSpeed) {
-					last = System.nanoTime();
-					tick();
+			if(!gameover) {
+//				System.out.println(P1.isDead()==P2.isDead());
+				if(P1.isDead()==true) {
+//					System.out.println(LocalPlayer.dead);
+					LocalPlayer.dead1=true;
+					gameover=true;
 				}
-//				if(snakes.get(0).dead==true) {
-//					this.dead=1;
-//				if(snakes.get(1).dead == true) {
-//					this.dead = 2;
+				if(P2.isDead() == true) {
+
+//					System.out.println(LocalPlayer.dead);
+					LocalPlayer.dead2 = true;
+					gameover=true;
+				}
+//				if(P2.isDead() == true && P1.isDead()==true) {
+//
+////					System.out.println(LocalPlayer.dead);
+//					LocalPlayer.dead2 = true;
+//					LocalPlayer.dead1=true;
+//					gameover=true;
 //				}
-//				}
+				if(!paused) {
+					if(System.nanoTime() - last > gameSpeed) {
+						last = System.nanoTime();
+						tick();
+					}
+				}
 			}
 		}
 	}
 	
 	private void tick() {
 		
-		snakes.get(0).tick();
-		snakes.get(1).tick();
+//		snakes.get(0).tick();
+//		snakes.get(1).tick();
+		P1.tick();
+		P2.tick();
 		food.tick();
 		
-		if(snakes.get(0).checkCollisionWith(food)) {
+		if(P1.checkCollisionWith(food)) {
 			food = new Food();
 		}
-		if(snakes.get(1).checkCollisionWith(food)) {
+		if(P2.checkCollisionWith(food)) {
 			food = new Food();
 		}
-		if(snakes.get(0).checkCollisionWith(snakes.get(1))) {
+		if(P1.checkCollisionWith(P2)) {
 			food = new Food();
 		}
-		if(snakes.get(1).checkCollisionWith(snakes.get(0))) {
+		if(P2.checkCollisionWith(P1)) {
 			food = new Food();
 		}
 	}
 	
-	public Snake getSnake(int x) {
-		
-		return snakes.get(x);
-	}
+//	public Snake getSnake(int x) {
+//		
+//		return snakes.get(x);
+//	}
 	
 	public Food getFood() {
 		
